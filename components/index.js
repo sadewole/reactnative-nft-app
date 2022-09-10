03c6bd6bbf36eacdfe342ad73980e61bb2377193
sadewole <samueladewole15@gmail.com>
2022-09-10 01:30:55 +0100

    created nft-card and also sub-components and designs

---
+++ components/index.js
@@ -0,0 +1,4 @@
+export { default as FocusedStatusBar } from './FocusedStatusBar';
+export { default as HomeHeader } from './HomeHeader';
+export { default as NFTCard } from './NFTCard';
+export { CircleButton, RectButton } from './Button';
