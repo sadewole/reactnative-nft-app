1e93a9e9b7a803237652a959306b4e617096b62a
sadewole <samueladewole15@gmail.com>
2022-09-10 02:30:03 +0100

    designing details screen header and description

--- components/index.js
+++ components/index.js
@@ -1,4 +1,7 @@
 export { default as FocusedStatusBar } from './FocusedStatusBar';
 export { default as HomeHeader } from './HomeHeader';
 export { default as NFTCard } from './NFTCard';
-export { CircleButton, RectButton } from './Button';
+export * from './Button';
+export * from './SubInfo';
+export { default as DetailsDesc } from './DetailsDesc';
+export { default as DetailsBid } from './DetailsBid';
